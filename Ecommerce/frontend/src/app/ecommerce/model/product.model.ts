df93882362427b999de62b84c1990c5b641d6656
Minh Duc Ngo <wunsch85@gmail.com>
2022-05-18 12:36:12 +0200

    Product component

--- Ecommerce/frontend/src/app/ecommerce/model/product.model.ts
+++ Ecommerce/frontend/src/app/ecommerce/model/product.model.ts
@@ -1,13 +1,13 @@
 export class Product {
-    id: number;
-    name: string;
-    price: number;
-    pictureUrl: string;
+  id: number;
+  name: string;
+  price: number;
+  pictureUrl: string;
 
-    constructor(id: number, name: string, price: number, pictureUrl: string) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-        this.pictureUrl = pictureUrl;
-    }
+  constructor(id: number, name: string, price: number, pictureUrl: string) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.pictureUrl = pictureUrl;
+  }
 }
--- Ecommerce/frontend/src/app/ecommerce/products/products.component.ts
+++ Ecommerce/frontend/src/app/ecommerce/products/products.component.ts
@@ -1,3 +1,4 @@
+import { ProductOrders } from './../model/product-orders.model';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ProductOrder } from '../model/product-order.model';
@@ -12,6 +13,11 @@ import { EcommerceService } from '../service/ecommerce.service';
 export class ProductsComponent implements OnInit {
   productOrders: ProductOrder[] = [];
   products: Product[] = [];
+  selectedProductOrder: ProductOrder;
+  productSelected: boolean = false;
+
+  private shoppingCartOrders: ProductOrders;
+
   sub: Subscription;
 
   constructor(private ecommerceService: EcommerceService) {
@@ -24,7 +30,7 @@ export class ProductsComponent implements OnInit {
   }
 
   loadProducts() {
-    this.ecommerceService.getAllProducts().subscribe((products : any) => {
+    this.ecommerceService.getAllProducts().subscribe((products: any) => {
       this.products = products;
       this.products.forEach(product => {
         this.productOrders.push(new ProductOrder(product, 0));
@@ -32,11 +38,43 @@ export class ProductsComponent implements OnInit {
     });
   }
 
+  getProductIndex(product: Product): number {
+    return this.ecommerceService.ProductOrders.productOrders.findIndex(
+      value => value.product === product);
+  }
+
+  isProductSelected(product: Product): boolean {
+    return this.getProductIndex(product) > -1;
+  }
+
   loadOrders() {
     this.sub = this.ecommerceService.OrdersChanged.subscribe(() => {
-
+      this.shoppingCartOrders = this.ecommerceService.ProductOrders;
     });
   }
 
+  addToCart(productOrder: ProductOrder) {
+    console.log("addTocart: " + JSON.stringify(productOrder))
+    this.ecommerceService.SelectedProductOrder = productOrder;
+    this.selectedProductOrder = this.ecommerceService.SelectedProductOrder;
+    this.productSelected = true;
+  }
 
+  removeFromCart(productOrder: ProductOrder) {
+    let index = this.getProductIndex(productOrder.product);
+    if (index > -1) {
+      this.shoppingCartOrders.productOrders.splice(this.getProductIndex(productOrder.product), 1);
+    }
+    this.ecommerceService.ProductOrders = this.shoppingCartOrders;
+    this.shoppingCartOrders = this.ecommerceService.ProductOrders;
+    this.productSelected = false;
+  }
+
+  reset() {
+    this.productOrders = [];
+    this.loadProducts();
+    this.ecommerceService.ProductOrders.productOrders = [];
+    this.loadOrders();
+    this.productSelected = false;
+  }
 }
