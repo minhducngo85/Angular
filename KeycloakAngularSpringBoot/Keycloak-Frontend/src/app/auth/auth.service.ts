938425902d0bb4ff84e12f15f9dfaa31d910126e
Minh Duc Ngo <wunsch85@gmail.com>
2023-01-19 17:03:07 +0100

    add auth guard and service

---
+++ KeycloakAngularSpringBoot/Keycloak-Frontend/src/app/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private keycloakService: KeycloakService) { }
+
+  getUsername(): string {
+    return this.keycloakService.getUsername();
+  }
+
+  logout(): void {
+    this.keycloakService.logout();
+  }
+
+  getLoggedUser() : any {
+    return this.keycloakService.getKeycloakInstance().idTokenParsed;
+  } 
+}
