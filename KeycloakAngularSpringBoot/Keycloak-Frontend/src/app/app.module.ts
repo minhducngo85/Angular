f92d01525f75f672279f14cb8f7ab7a6ff59facc
Minh Duc Ngo <wunsch85@gmail.com>
2023-01-19 16:32:10 +0100

    step 3 - setup app-init.js and app-module.ts

--- KeycloakAngularSpringBoot/Keycloak-Frontend/src/app/app.module.ts
+++ KeycloakAngularSpringBoot/Keycloak-Frontend/src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializer } from 'src/utils/app-init';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    KeycloakAngularModule  // add keycloakAngular module
   ],
-  providers: [],
+  providers: [
+     // add this provider
+     {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      deps: [KeycloakService],
+      multi: true,
+    },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
